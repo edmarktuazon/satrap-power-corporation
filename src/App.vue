5973f2bd604e0d32d07cb77e94f6e622ade748f6
edmarktuazon <edmarktuazon03@gmail.com>
2025-10-16 12:24:08 +0800

    dynamically loop the router link and add resize func

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import { toRefs, reactive, ref } from 'vue'
+import { reactive, ref, onMounted, onUnmounted } from 'vue'
 
-const navLinks = reactive({
-  home: 'Home',
-  about: 'About',
-  services: 'Services',
-  testimonials: 'Testimonials',
-  contact: 'Contact',
-})
-
-const { home, about, services, testimonials, contact } = toRefs(navLinks)
+const navLinks = reactive([
+  { name: 'Home', to: '/' },
+  { name: 'About', to: '/about' },
+  { name: 'Services', to: '/services' },
+  { name: 'Testimonials', to: '/testimonials' },
+  { name: 'Contact', to: '/contact' },
+])
 
 const isOpen = ref(false)
 
@@ -21,6 +19,20 @@ const toggleMenu = () => {
 const closeMenu = () => {
   isOpen.value = false
 }
+
+const handleResize = () => {
+  if (window.innerWidth >= 1024) {
+    isOpen.value = false
+  }
+}
+
+onMounted(() => {
+  window.addEventListener('resize', handleResize)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', handleResize)
+})
 </script>
 
 <template>
@@ -31,12 +43,10 @@ const closeMenu = () => {
 
     <!-- Desktop nav -->
     <nav>
-      <ul class="space-x-8 font-medium text-secondary hidden lg:block">
-        <RouterLink to="/">{{ home }}</RouterLink>
-        <RouterLink to="/about">{{ about }}</RouterLink>
-        <RouterLink to="/services">{{ services }}</RouterLink>
-        <RouterLink to="/testimonials">{{ testimonials }}</RouterLink>
-        <RouterLink to="/contact">{{ contact }}</RouterLink>
+      <ul class="space-x-8 font-medium text-secondary lg:flex hidden">
+        <li v-for="link in navLinks" :key="link.to">
+          <RouterLink :to="link.to" class="hover:text-primary">{{ link.name }}</RouterLink>
+        </li>
       </ul>
     </nav>
 
@@ -44,36 +54,36 @@ const closeMenu = () => {
     <transition name="fade">
       <ul
         v-if="isOpen"
-        class="absolute top-[4.375rem] left-0 w-full bg-white shadow-md flex flex-col items-start py-6 px-10 space-y-4 font-medium text-secondary lg:hidden"
+        class="absolute top-[4.375rem] left-0 w-full bg-white shadow-md rounded-b-lg flex flex-col items-start py-6 px-10 space-y-4 font-medium text-secondary lg:hidden"
       >
-        <RouterLink to="/" @click="closeMenu">{{ home }}</RouterLink>
-        <RouterLink to="/about" @click="closeMenu">{{ about }}</RouterLink>
-        <RouterLink to="/services" @click="closeMenu">{{ services }}</RouterLink>
-        <RouterLink to="/testimonials" @click="closeMenu">{{ testimonials }}</RouterLink>
-        <RouterLink to="/contact" @click="closeMenu">{{ contact }}</RouterLink>
+        <li v-for="link in navLinks" :key="link.to">
+          <RouterLink :to="link.to" @click="closeMenu" class="hover:text-primary">{{
+            link.name
+          }}</RouterLink>
+        </li>
       </ul>
     </transition>
 
-    <div
+    <button
       @click="toggleMenu"
       aria-label="Toggle Menu"
       class="relative w-6 h-5 flex flex-col justify-between cursor-pointer lg:hidden"
     >
       <span
-        class="absolute top-[0.0313rem] left-0 w-6 h-0.5 bg-secondary transition-all duration-300"
-        :class="{ 'rotate-45 translate-y-2': isOpen }"
+        class="absolute top-[0.125rem] left-0 w-5 h-0.5 bg-primary transition-all duration-300 rounded-full"
+        :class="{ 'rotate-45 translate-y-[0.5rem]': isOpen }"
       ></span>
 
       <span
-        class="absolute top-2 left-0 w-6 h-0.5 bg-secondary transition-all duration-300"
+        class="absolute top-2 left-0 w-5 h-0.5 bg-primary transition-all duration-300 rounded-full"
         :class="{ 'opacity-0': isOpen }"
       ></span>
 
       <span
-        class="absolute bottom-[0.125rem] left-0 w-6 h-0.5 bg-secondary transition-all duration-300"
-        :class="{ '-rotate-45 -translate-y-2': isOpen }"
+        class="absolute bottom-[0.25rem] left-0 w-5 h-0.5 bg-primary transition-all duration-300 rounded-full"
+        :class="{ '-rotate-45 -translate-y-1': isOpen }"
       ></span>
-    </div>
+    </button>
   </header>
 
   <RouterView />
