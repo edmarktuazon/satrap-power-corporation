1030c6947b62fffad78eb20c131b2f051424e8ca
edmarktuazon <edmarktuazon03@gmail.com>
2025-10-16 20:38:10 +0800

    pre commit

--- src/components/HeroSectionWrapper.vue
+++ src/components/HeroSectionWrapper.vue
@@ -1,14 +1,21 @@
 <template>
-  <div
+  <section
     class="bg-[url('https://images.pexels.com/photos/3747075/pexels-photo-3747075.jpeg')] bg-center bg-cover bg-no-repeat h-screen w-full"
   >
-    <div class="flex flex-col items-center justify-center">
-      <h1 class="text-sm md:text-lg text-white mb-2">
-        <slot name="heading"></slot>
-      </h1>
-      <p class="text-lg md:text-2xl mb-8 text-white">
-        <!-- <slot name="paragraph"></slot> -->
-      </p>
+    <div class="container">
+      <div class="three-column-grid">
+        <div class="flex flex-col items-center justify-center">
+          <h1 class="text-sm md:text-lg text-white mb-2">
+            <slot name="hero-heading"></slot>
+          </h1>
+          <p class="text-lg md:text-2xl mb-8 text-white">
+            <slot name="hero-paragraph"></slot>
+          </p>
+          <button>
+            <slot name="hero-button"></slot>
+          </button>
+        </div>
+      </div>
     </div>
-  </div>
+  </section>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup>
-import HeroSection from '../components/HeroSection.vue'
-import AboutSection from '../components/AboutSection.vue'
+import HeroSection from '@/components/HeroSection.vue'
+import AboutSection from '@/components/AboutSection.vue'
 </script>
 
 <template>
