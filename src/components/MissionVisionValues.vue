692068913b745c6e2803274b2d6913e9a8c5c557
edmarktuazon <edmarktuazon03@gmail.com>
2025-10-19 12:40:10 +0800

    render json data using component

--- src/components/MissionVisionValues.vue
+++ src/components/MissionVisionValues.vue
@@ -1,12 +1,9 @@
 <script setup>
-// import companyInfo from '@/data/companyInfo.json'
-import MissionVisionValues from './MissionVisionValues.vue'
+import companyInfo from '@/data/companyInfo.json'
 </script>
 
 <template>
-  <div
-    class="bg-[url('https://images.pexels.com/photos/3747075/pexels-photo-3747075.jpeg')] bg-center bg-cover bg-no-repeat h-screen w-full"
-  >
+  <div>
     <div class="flex flex-col items-center justify-center">
       <h1 class="text-sm md:text-lg text-white mb-2">
         <slot name="heading"></slot>
@@ -14,7 +11,10 @@ import MissionVisionValues from './MissionVisionValues.vue'
       <p class="text-lg md:text-2xl mb-8 text-white">
         <slot name="paragraph"></slot>
       </p>
-      <MissionVisionValues />
+      <div v-for="(item, index) in companyInfo" :key="index">
+        <h2>{{ item.title }}</h2>
+        <p>{{ item.content }}</p>
+      </div>
     </div>
   </div>
 </template>
