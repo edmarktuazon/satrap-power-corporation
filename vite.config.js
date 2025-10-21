ed06071e18255c1dc353545e9a7276422cf61c92
edmarktuazon <edmarktuazon03@gmail.com>
2025-10-21 18:57:05 +0800

    Removed VueDevTools from UI

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import VueDevTools from 'vite-plugin-vue-devtools'
+// import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), tailwindcss(), VueDevTools()],
+  // VueDevTools()
+  plugins: [vue(), vueJsx(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
