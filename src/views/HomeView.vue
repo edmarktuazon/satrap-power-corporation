872022b382f0798dd652c81947c093e5fe685bc2
edmarktuazon <edmarktuazon03@gmail.com>
2025-10-22 10:11:26 +0800

    Transfered GoToTopButton to components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,27 +3,13 @@ import HeroSection from '@/components/HeroSection.vue'
 import NavigationSection from '@/components/NavigationWrapper.vue'
 import AboutSection from '@/components/AboutSection.vue'
 import ProjectSection from '@/components/ProjectSection.vue'
-import ChevronUpIcon from '@/components/icons/ChevronUpIcon.vue'
 import FooterSection from '@/components/FooterSection.vue'
-
-import { useScroll } from '@/composables/useScroll'
-
-const { isActiveToTopButton, goToTop } = useScroll()
+import GoToTopButton from '@/components/buttons/GoToTopButton.vue'
 </script>
 
 <template>
   <main>
-    <div class="fixed bottom-5 right-5">
-      <transition name="fade">
-        <button
-          v-if="isActiveToTopButton"
-          @click="goToTop"
-          class="fixed bottom-5 right-5 bg-primary p-3 rounded-full cursor-pointer"
-        >
-          <ChevronUpIcon class="w-7 h-7 text-white" />
-        </button>
-      </transition>
-    </div>
+    <GoToTopButton />
     <NavigationSection />
     <HeroSection />
     <AboutSection />
@@ -31,18 +17,3 @@ const { isActiveToTopButton, goToTop } = useScroll()
     <FooterSection />
   </main>
 </template>
-
-<style>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.3s;
-}
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
-}
-.fade-enter-to,
-.fade-leave-from {
-  opacity: 1;
-}
-</style>
